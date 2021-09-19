727bae33da375d00b20ffe9f86e5d0d2bca2a4a4
Angga Suriana <anggasurianae65@gmail.com>
2021-09-18 17:00:39 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+  $nama = '';
+  $nama=$_GET['nama']; 
+?>
+<html>
+  <style>
+    #container {
+      text-align: center;
+    }
+  </style>
+  <h2 id="container">Selamat Datang <?php echo $nama; ?> di Website Kami</h2>
+</html>

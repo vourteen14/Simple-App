105b2c3572b891f8a1d3a81001269cce7eb57e95
Angga Suriana <angga.suriana.202@gmail.com>
2021-09-19 05:58:09 +0000

    Adding form on index.php

--- index.php
+++ index.php
@@ -8,5 +8,12 @@
       text-align: center;
     }
   </style>
-  <h2 id="container">Selamat Datang <?php echo $nama; ?> di Website Kami</h2>
+  <div id="container">
+    <h2>Selamat Datang <?php echo $nama; ?> di Website Kami</h2>
+	<form action="" method="get">
+	  <input type="text" name="nama">
+	  <br>
+	  <input type="submit" name="submit">
+	</form>
+  </div>
 </html>
